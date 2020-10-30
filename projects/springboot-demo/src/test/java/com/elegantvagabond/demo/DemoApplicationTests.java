f7af1f15a4f62c2f2f3f196c1f29ebdfdaa58034
EleganteVagbond <hpogt1@gmail.com>
2020-10-30 10:11:54 -0600

    Initial add

---
+++ projects/springboot-demo/src/test/java/com/elegantvagabond/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elegantvagabond.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
